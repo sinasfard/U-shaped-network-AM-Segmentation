4e8bec79e00b614d0ab5c964cd5a20a8035e6f90
Sina Soleimani Fard (Sina SFard) <91623372+sinasfard@users.noreply.github.com>
2024-10-21 14:19:51 -0600

    Create k_fold.py

---
+++ k_fold.py
@@ -0,0 +1,6 @@
+from torch.utils.data import ConcatDataset
+from sklearn.model_selection import KFold
+import sklearn.metrics as metrics
+
+def concat_data:
+   return ConcatDataset([train_dataset, val_dataset])
